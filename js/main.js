a268c95463a3fba3978bd712a99b64343521ac3e
mwimh <112586751+mwimh@users.noreply.github.com>
2023-08-14 17:08:20 -0500

    basic popup added

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ function createMap() {
     map = L.map('map', {
         center: [43.07, -89.4],
         zoom: 11,
-        maxZoom: 19,
+        maxZoom: 18,
         minZoom: 10,
         maxBounds: [
             //south west
@@ -82,21 +82,72 @@ function addFixedBoundaries(map) {
             parks = new L.geoJson(json, {
                 style: function (feature) {
                     return {
-                        fillColor: "none",
+                        fillColor: "green",
                         color: "green",
-                        weight: 2,
+                        weight: 3,
                         opacity: 0.8,
                         className: 'parks'
                     }
-                }
+                },
+                onEachFeature
             });
             parks.addTo(map)
+
+            console.log(parks)
         })
 
 
 }
 
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
 
+//highlight function
+function highlightFeature(e) {
+    const layer = e.target;
+
+    layer.setStyle({
+        weight: 6,
+        color: '#eaa40e',
+    });
+
+    e.target.bindPopup("I am a Park.");
+
+}
+
+function createPopup(e) {
+    e.target.bindPopup("I am a Park.");
+}
+
+
+//function to reset highlight to initial parameters
+function resetHighlight(e) {
+    parks.setStyle({
+        fillColor: "green",
+        color: "green",
+        weight: 3,
+        opacity: 0.8,
+    })
+}
+
+function zoomToFeature(e) {
+    //reset huc10 style initially to remove previous color change
+    //fly to the center of the selected huc10
+    parkCenter = e.target.getBounds().getCenter();
+    map.flyTo(parkCenter, 15);
+}
+
+function PopupContent(properties, attribute) {
+    this.properties = properties;
+    this.attribute = attribute;
+    this.employment = properties[attribute];
+    this.formatted = "<p><b>Country:</b> " + this.properties.Country + "</p><p><b>" + "Industry Emp. in " + this.year + ":</b> " + Math.round(this.properties[attribute] * 10) / 10 + "%</p>" + "<p><b>28-Year Change:</b> " + this.change + "%</p>";
+};
 
 // Create the map once everything is loaded
 document.addEventListener('DOMContentLoaded', createMap);
