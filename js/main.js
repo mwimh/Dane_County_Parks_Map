3ac99fd83188952cac36506eac08f5bf8ad33f4e
mwimh <112586751+mwimh@users.noreply.github.com>
2023-08-14 13:57:43 -0500

    basemap

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
-// Add all scripts to the JS folder
+// Declare global variables
+var map;
+var dataStats = {};
+
+
+// Create the basemap
+function createMap() {
+    map = L.map('map', {
+        center: [43.10, -89.4],
+        zoom: 11,
+        maxZoom: 19,
+        minZoom: 10,
+        maxBounds: [
+            //south west
+            [42.5, -91],
+            //north east
+            [44, -88]
+        ],
+        maxBoundsViscosity: 1.0,
+    });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
+    }).addTo(map);
+
+    map.invalidateSize()
+
+    //getData();
+};
+
+
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(feature.properties.popupContent);
+    }
+}
+
+
+
+
+
+
+// Create the map once everything is loaded
+document.addEventListener('DOMContentLoaded', createMap);
